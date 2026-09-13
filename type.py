85f391715aa74a8868a69da3282687dbb68f8c7a
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-13 23:10:53 +0600

    types

--- type.py
+++ type.py
@@ -2,21 +2,20 @@
 def add(x: int, y: int) -> int:
     return x + y
 
-# 2
-# ১. শুধু সংখ্যা বা স্ট্রং-এর লিস্ট (TS-এর number[] বা string[])
+# 2.list
+
 numbers: list[int] = [1, 2, 3, 4]
 names: list[str] = ["Ali", "Rafi"]
 
-# ২. ডিকশনারি (TS-এর Record<string, number> বা Object)
-# এখানে Key হবে string এবং Value হবে int
+# 3.dist
 user_scores: dict[str, int] = {"Ali": 95, "Rafi": 88}
 
-# ৩. একাধিক টাইপ সাপোর্ট করা বা Union Type (TS-এর string | number)
-# পাইথনে এর জন্য পাইপ `|` সিম্বল ব্যবহার করা হয়
-id_code: str | int = 101
-id_code = "A101" # দুটিই সঠিক
+# string | number)
 
-# ৪. কোনো কিছু রিটার্ন না করলে (TS-এর void)
+id_code: str | int = 101
+id_code = "A101" 
+
+# func
 def greet(name: str) -> None:
     print(f"Hello {name}")
 
