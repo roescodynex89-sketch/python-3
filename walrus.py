e8779f84fdaff984987bb502d65c41116c68f8b8
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-13 23:08:27 +0600

    python

---
+++ walrus.py
@@ -0,0 +1,15 @@
+# before
+
+
+# length = len("Python") 
+# if length > 5: 
+#     print(f"Too long! Length is {length}")
+
+
+
+# walrus use 
+
+if (length := len("Python")) > 5:
+    print(f"Too long! Length is {length}")
+
+    
