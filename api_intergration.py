ac695a14a67d963d3e734dc51dd189a7fc650be2
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-14 20:25:41 +0600

    api

--- api_intergration.py
+++ api_intergration.py
@@ -1,15 +1,15 @@
 import requests
 
-# ১. API এর URL (আমরা একটি ফ্রি ফেক API ব্যবহার করছি)
-BASE_URL = "https://typicode.com"
+# api :https://api.mydummyapi.com/comments/1
+BASE_URL = "https://api.mydummyapi.com/comments/1"
 
 
-# --- ক. GET Request (ডাটা নিয়ে আসা) ---
+#GET
 def get_post_data(post_id: int) -> None:
     response = requests.get(f"{BASE_URL}/{post_id}")
     
     if response.status_code == 200:
-        # response.json() অটোমেটিক JSON-কে পাইথন ডিকশনারি বানিয়ে দেয়
+        # response.json()  JSON DICT
         data = response.json() 
         print("--- GET Success ---")
         print(f"Title: {data['title']}\n")
@@ -17,7 +17,7 @@ def get_post_data(post_id: int) -> None:
         print("Failed to fetch data")
 
 
-# --- খ. POST Request (নতুন ডাটা পাঠানো) ---
+# ---  POST Request ---
 def create_new_post(title: str, body: str) -> None:
     payload = {
         "title": title,
@@ -25,16 +25,13 @@ def create_new_post(title: str, body: str) -> None:
         "userId": 1
     }
     
-    # json= প্যারামিটার দিলে requests লাইব্রেরি নিজেই ডিকশনারিকে JSON বানিয়ে পাঠায়
+    # json= When parameters are provided, the `requests` library automatically converts the dictionary into JSON and sends it.
     response = requests.post(BASE_URL, json=payload)
     
-    if response.status_code == 201: # 201 মানে Created
+    if response.status_code == 201: # 201  Created
         print("--- POST Success ---")
         print("Server Response:", response.json())
     else:
         print("Failed to send data")
 
 
-# ফাংশনগুলো রান করে দেখা
-get_post_data(1)
-create_new_post("Learning API Integration", "It is very straightforward in Python!")
